e1c474992d9a9be67002528151aa147bbe332011
elin6776 <164929726+elin6776@users.noreply.github.com>
2024-03-25 14:58:29 -0400

    Add files via upload

---
+++ Website CSS/script.js
@@ -0,0 +1,9 @@
+function myDropdownFunction() {
+    var linksDiv = document.getElementById("theLinks");
+    if (linksDiv.style.display === "block") {
+      linksDiv.style.display = "none";
+    }
+      else {
+      linksDiv.style.display = "block";
+    }
+  }
