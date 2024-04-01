0b0a502a9782d0be4a62fdadaffd2187d4a6cd51
elin6776 <164929726+elin6776@users.noreply.github.com>
2024-04-01 11:26:46 -0400

    Delete Website CSS directory

--- Website CSS/script.js
+++
@@ -1,9 +0,0 @@
-function myDropdownFunction() {
-    var linksDiv = document.getElementById("theLinks");
-    if (linksDiv.style.display === "block") {
-      linksDiv.style.display = "none";
-    }
-      else {
-      linksDiv.style.display = "block";
-    }
-  }
